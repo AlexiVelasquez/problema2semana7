8c18c962b615fec85d727834d9b6219201b54806
alexi <alexi@LAPTOP-K1LI30UL>
2023-10-04 23:28:04 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace problema2semana7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1; 
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("Divisor de Omar Orlandini " + contador + ": ");
+                    Console.WriteLine(i);
+                    contador++;
+                    
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
